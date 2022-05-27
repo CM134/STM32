cb7aa8c0a31bb754883fe3cde6be0839d7498cfe
mhh469 <80479680+mhh469@users.noreply.github.com>
2022-05-27 12:23:56 +0200

    FreeRTOS

---
+++ FreeRTOS_Projects/Debug/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.d
@@ -0,0 +1,29 @@
+ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.o: \
+ ../ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.c \
+ C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h
+
+C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+
+C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+
+C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h:
+
+C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h:
+
+C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+
+C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+
+C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h:
+
+C:/Users/mikke/Desktop/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h:
